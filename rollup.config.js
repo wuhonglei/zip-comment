938738b150f03ff31ea01f636c1d3da0969739e4
wuhonglei <1017368065@qq.com>
2020-09-03 00:29:51 +0800

    打包输出 terser 压缩后的代码

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,5 @@
+import { terser } from 'rollup-plugin-terser';
+
 export default {
     input: 'src/main.js',
     output: [{
@@ -5,7 +7,20 @@ export default {
         format: 'umd',
         name: 'loadZipAsync'
     }, {
+        file: './dist/bundle.umd.min.js',
+        format: 'umd',
+        name: 'loadZipAsync',
+        plugins: [
+            terser()
+        ]
+    }, {
         file: './dist/bundle.esm.js',
         format: 'esm'
+    }, {
+        file: './dist/bundle.esm.min.js',
+        format: 'esm',
+        plugins: [
+            terser()
+        ]
     }]
 };
